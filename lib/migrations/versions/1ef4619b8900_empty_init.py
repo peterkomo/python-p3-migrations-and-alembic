f0f7600f86a4d3a0528895967c36eb14d5ad292a
peterkomo <mwangipeterkomo@gmail.com>
2023-09-01 08:10:32 +0300

    complited assignment

---
+++ lib/migrations/versions/1ef4619b8900_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 1ef4619b8900
+Revises: 
+Create Date: 2023-09-01 08:07:31.825922
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1ef4619b8900'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
